b88a1f99a55992e838ffaa44e743a344e98f51b0
wisnermarques <76441538+wisnermarques@users.noreply.github.com>
2022-01-01 15:41:30 -0300

    Arquivos do projeto local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe('Suite de teste de exemplo com NodeJS', function() {
+    it('deve demonstrar o uso do Jasmine com NodeJS', function() {
+        expect(true).toEqual(true)
+    })
+
+    it('deve demonstrar o uso do karma em modo de escuta', function() {
+        expect(1).toEqual(1)
+    })
+})
